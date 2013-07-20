ee877a58c29d358fb16afe5bffb4ba936ac2c7ee
wangqingpeng <wangqingpeng@douban.com>
2013-07-20 19:47:09 +0800

    update templates

--- plant/views.py
+++ plant/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from plant.models import Plant
 
-def home(request, plant_id):
+def home(request):
+    plants = Plant.objects.all()
+    data = {'plants': plants}
+    return render_to_response('index.html', data,
+            context_instance=RequestContext(request))
+
+
+def get_plant(request, plant_id):
     plant = Plant.objects.get(id=plant_id)
     data = {'plant': plant}
-    return render_to_response('index.html', data,
+    return render_to_response('plant.html', data,
             context_instance=RequestContext(request))
